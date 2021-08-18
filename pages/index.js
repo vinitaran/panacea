7a6696bdc24ed6107388e9acc234602c94174bc6
Abhishek <vijayanabhishek1912@gmail.com>
2021-08-18 14:34:06 +0530

    Logo Component

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import EmailButton from '../components/EmailButton'
+import Logo from '../components/Logo'
 
 export default function Home() {
   return (
     <div>
+      
       <Head>
         <title>PANACEA 2021</title>
         <meta name="description" content="Asia Asexuality Conference 2021" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Panacea</h1>
+      <h1><Logo />Panacea</h1>
       <EmailButton />
     </div>
   )
