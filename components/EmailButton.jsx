0cc3f71ad68fe6d6eaf96bd17c879d3bce609def
Vinita <vinita.rane@periscope-tech.com>
2021-08-18 12:11:38 +0530

    sass setup

---
+++ components/EmailButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function EmailButton() {
+    return (
+        <>
+            <button className="emailButton__button">Send</button>
+        </>
+    )
+}
