a698ef06e9b3e85beed05c552a9ae4346f8f8b35
Abhishek <vijayanabhishek1912@gmail.com>
2021-08-19 11:30:23 +0530

    Gradient @

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,18 @@ import Logo from '../components/Logo'
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="mainBody"> 
       <Head>
         <title>PANACEA 2021</title>
         <meta name="description" content="Asia Asexuality Conference 2021" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1><Logo />Panacea</h1>
+      
+      <h1>Panacea</h1>
+      
       <EmailButton />
+      <Logo />
+      
     </div>
   )
 }
