ac40a61183b983bfd370387d5f944c9050399d60
Abhishek <vijayanabhishek1912@gmail.com>
2021-08-20 09:53:09 +0530

    Background Image

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import EmailButton from '../components/EmailButton'
-import Logo from '../components/Logo'
+import Head from "next/head";
+import Image from "next/image";
+import EmailButton from "../components/EmailButton";
+import Logo from "../components/Logo";
 
 export default function Home() {
-  return (
-    <div className="mainBody"> 
-      <Head>
-        <title>PANACEA 2021</title>
-        <meta name="description" content="Asia Asexuality Conference 2021" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      
-      <h1>Panacea</h1>
-      
-      <EmailButton />
-      <Logo />
-      
-    </div>
-  )
+	return (
+		<div>
+			<Head>
+				<title>PANACEA 2021</title>
+				<meta name='description' content='Asia Asexuality Conference 2021' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<div className='mainBody'>
+			<div className='imageHolder'>
+			{/* <h1>
+					PAN<span className='aceText'>ACE</span>A
+				</h1>
+
+				<EmailButton /> */}
+			</div>
+				
+				
+			</div>
+		</div>
+	);
 }
