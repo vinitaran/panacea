fee337ba971de90bc9bcb268a8b1ad2cb857b94d
Abhishek <vijayanabhishek1912@gmail.com>
2021-08-22 12:43:03 +0530

    Layout and Text

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -9,6 +9,17 @@ const HomePage = () => {
 				<h1>
 					PAN<span className='aceText'>ACE</span>A
 				</h1>
+				<h2 id='subheading'>Asexuality Asia Conference 2021</h2>
+			</div>
+			<div className='homeContent'>
+				<h2 id='date'>October 25th 2021 - October 31st 2021</h2>
+				<h3 id='tagLine'>
+					World's first conference dedicated to the asexual community of Asia!
+				</h3>
+				<h4>
+					Contact for queries,partnerships and collaboration:{" "}
+					<a href=''>asexuality.asia@gmail.com</a>{" "}
+				</h4>
 			</div>
 			<div className='home__footer'>
 				<EmailButton />
--- components/SocialMediaIcons.jsx
+++ components/SocialMediaIcons.jsx
@@ -4,10 +4,15 @@ import { FaFacebook, FaDiscord, FaInstagram, FaTwitter } from "react-icons/fa";
 export default function SocialMediaIcons() {
 	return (
 		<div className='socialIcons'>
-			<FaFacebook className="media__icons" />
-			<FaDiscord className="media__icons" />
-			<FaInstagram className="media__icons" />
-			<FaTwitter className="media__icons" />
+			<div>
+				<FaFacebook className='media__icons' />
+				<FaDiscord className='media__icons' />
+				<FaInstagram className='media__icons' />
+				<FaTwitter className='media__icons' />
+			</div>
+			<div>
+				<h4>Organised By Indian Aces</h4>
+			</div>
 		</div>
 	);
 }
