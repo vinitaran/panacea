a56a719232d56e992a34ebf391dfc978d36b35bd
Vinita <vinita.rane@periscope-tech.com>
2021-08-22 09:38:07 +0530

    background

--- pages/index.js
+++ pages/index.js
@@ -12,19 +12,9 @@ export default function Home() {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<div className='mainBody'>
-			<div className='imageHolder'>
-			<h1>
-					PAN<span className='aceText'>ACE</span>A
-				</h1>
-
-				<EmailButton />
-				<SocialMediaIcons />	
-			</div>
-
 			<img src="https://media.wired.com/photos/5e618502dd088e00081102a9/2:1/w_2400,h_1200,c_limit/Biz-conference-187591171.jpg" />
-				
-			
 			</div>
+				
 		</div>
 	);
 }
