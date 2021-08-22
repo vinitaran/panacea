e2bcabce7bf797ea1680308dc3e94f6db60a933e
Abhishek <vijayanabhishek1912@gmail.com>
2021-08-22 09:09:59 +0530

    Icons

--- components/SocialMediaIcons.jsx
+++ components/SocialMediaIcons.jsx
@@ -1,2 +1,14 @@
 import React from "react";
+import { FaFacebook, FaDiscord, FaInstagram, FaTwitter } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
 
+export default function SocialMediaIcons() {
+	return (
+		<div className='socialIcons'>
+			<FaFacebook></FaFacebook>
+			<FaDiscord></FaDiscord>
+			<FaInstagram></FaInstagram>
+			<FaTwitter></FaTwitter>
+		</div>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
 import EmailButton from "../components/EmailButton";
-import Logo from "../components/Logo";
 import SocialMediaIcons from "../components/SocialMediaIcons";
 
 export default function Home() {
@@ -13,17 +12,18 @@ export default function Home() {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<div className='mainBody'>
-			{/* <div className='imageHolder'>
+			<div className='imageHolder'>
 			<h1>
 					PAN<span className='aceText'>ACE</span>A
 				</h1>
 
 				<EmailButton />
-			</div> */}
+				<SocialMediaIcons />	
+			</div>
 
 			<img src="https://media.wired.com/photos/5e618502dd088e00081102a9/2:1/w_2400,h_1200,c_limit/Biz-conference-187591171.jpg" />
 				
-			<SocialMediaIcons />	
+			
 			</div>
 		</div>
 	);
