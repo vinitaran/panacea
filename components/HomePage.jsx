4634da9824748705ea39bbe1f8659c9d668de2aa
Abhishek <vijayanabhishek1912@gmail.com>
2021-08-29 12:59:19 +0530

    General

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -2,44 +2,42 @@ import React, { useRef, useState, useEffect } from "react";
 import EmailButton from "./EmailButton";
 import SocialMediaIcons from "./SocialMediaIcons";
 import CountDownTimer from "./CountDownTimer";
-import {motion} from 'framer-motion';
-
+import { motion } from "framer-motion";
 
 const HomePage = () => {
 	const asiaRef = useRef();
 	return (
 		<div className='home__main'>
-			<div className='homeTitle'>			
+			<div className='homeTitle'>
 				<h1>
 					PAN
-				<motion.span
-					style={{ 
-						opacity: 0,
-						marginLeft: -100,
-					}}
-					animate={{
-						opacity: 1,
-						marginLeft: 0,
-					}}
-					transition={{
-						type: "spring",
-						stiffness: 60,
-						yoyo: Infinity,
-						duration: 1.1,
-					}}
-					className='aceText'
-				></motion.span>A
+					<motion.span
+						style={{
+							opacity: 0,
+							marginLeft: -100,
+						}}
+						animate={{
+							opacity: 1,
+							marginLeft: 0,
+						}}
+						transition={{
+							type: "spring",
+							stiffness: 60,
+							yoyo: Infinity,
+							duration: 1.1,
+						}}
+						className='aceText'></motion.span>
+					A
 				</h1>
 				{/* <h1>PAN<span className="aceText"></span>A</h1> */}
 				<CountDownTimer />
-				<h2	id='subheading'>Asexuality Asia Conference 2021</h2>
+				<h2 id='subheading'>Asexuality Asia Conference 2021</h2>
 			</div>
-			
+
 			<div className='homeContent'>
 				<h2 id='date'>October 25th - October 31st, 2021</h2>
 				<h3 id='tagLine'>
-					World's first virtual conference dedicated to the asexual community of
-					Asia!
+					World's first conference dedicated to the asexual community of Asia!
 				</h3>
 				<h4 id='contact'>
 					Contact for queries, partnerships and collaboration:{" "}
